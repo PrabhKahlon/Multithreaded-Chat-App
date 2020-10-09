04d80976ce9073dcb4fa492f6899394f1f65568d
prabhkahlon99 <prabh88a@hotmail.com>
2020-10-09 02:15:53 -0700

    added basic main.c and .gitignore

---
+++ main.c
@@ -0,0 +1,23 @@
+//main.c
+//NATHAN! Please do these "{" like this 
+/*
+    int function
+    {
+
+    }
+*/
+//AND not like this 
+/*
+    int function() {
+
+    }
+*/
+//THANKS
+//:D
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    printf("Hello World!\n");
+    return 0;
+}
